8af868a77feeb4d0201351c514296ce03a5b4782
Dee-dayo <darhyor2050@gmail.com>
2024-02-06 06:23:26 +0100

    added more testt

--- bikeApp.py
+++ bikeApp.py
@@ -1,9 +1,23 @@
 class Bike:
     def __init__(self):
         self.is_on = False
+        self.speedometer = 0
 
     def check_status(self):
         return self.is_on
 
     def start(self):
         self.is_on = True
+
+    def stop(self):
+        self.is_on = False
+
+    def check_speedometer(self):
+        return self.speedometer
+
+    def accelerate(self):
+        if self.is_on:
+            self.speedometer += 1
+
+
+
--- bikeAppTest.py
+++ bikeAppTest.py
@@ -3,9 +3,39 @@ import bikeApp
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_bike_turns_on(self):
         my_bike = bikeApp.Bike()
         self.assertFalse(my_bike.check_status())
 
         my_bike.start()
         self.assertTrue(my_bike.check_status())
+
+    def test_turn_bike_off_bike_turn_off(self):
+        my_bike = bikeApp.Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        self.assertTrue(my_bike.check_status())
+
+        my_bike.stop()
+        self.assertFalse(my_bike.check_status())
+
+    def test_accelerate_bike(self):
+        my_bike = bikeApp.Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        self.assertTrue(my_bike.check_status())
+
+        my_bike.accelerate()
+        self.assertEqual(1, my_bike.check_speedometer())
+
+        my_bike.accelerate()
+        my_bike.accelerate()
+        self.assertEqual(3, my_bike.check_speedometer())
+
+    def test_accelerate_when_bike_off(self):
+        my_bike = bikeApp.Bike()
+        self.assertFalse(my_bike.check_status())
+
+        my_bike.accelerate()
+        self.assertEqual(0, my_bike.check_speedometer())
