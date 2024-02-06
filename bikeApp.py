d7b35e6aca46e6ad1b2e6c916395efab64033fb0
Dee-dayo <darhyor2050@gmail.com>
2024-02-06 23:42:56 +0100

    added more testt

--- bikeApp.py
+++ bikeApp.py
@@ -27,9 +27,24 @@ class Bike:
             elif 31 <= self.check_speedometer() <= 40:
                 self.speedometer += 3
                 self.gear = 3
+            elif 41 <= self.check_speedometer():
+                self.speedometer += 4
+                self.gear = 4
 
     def check_gear(self):
         return self.gear
 
-
-
+    def brake(self):
+        if self.is_on:
+            if 0 <= self.check_speedometer() <= 20:
+                self.speedometer -= 1
+                self.gear = 1
+            elif 21 <= self.check_speedometer() <= 30:
+                self.speedometer -= 2
+                self.gear = 2
+            elif 31 <= self.check_speedometer() <= 40:
+                self.speedometer -= 3
+                self.gear = 3
+            elif 41 <= self.check_speedometer():
+                self.speedometer -= 4
+                self.gear = 4
--- bikeAppTest.py
+++ bikeAppTest.py
@@ -77,3 +77,68 @@ class MyTestCase(unittest.TestCase):
         my_bike.accelerate()
         self.assertEqual(3, my_bike.check_gear())
         self.assertEqual(34, my_bike.check_speedometer())
+
+    def test_bike_on_gear_1_speed_reduce_by_1(self):
+        my_bike = Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        self.assertEqual(0, my_bike.check_gear())
+        for count in range(15):
+            my_bike.accelerate()
+        self.assertEqual(15, my_bike.check_speedometer())
+
+        my_bike.brake();
+        self.assertEqual(14, my_bike.check_speedometer())
+
+        my_bike.brake()
+        my_bike.brake()
+        self.assertEqual(12, my_bike.check_speedometer())
+
+    def test_bike_on_gear_2_speed_reduce_by_2(self):
+        my_bike = Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        for count in range(25):
+            my_bike.accelerate()
+        self.assertEqual(29, my_bike.check_speedometer())
+        self.assertEqual(2, my_bike.check_gear())
+
+        my_bike.brake()
+        my_bike.brake()
+        self.assertEqual(25, my_bike.check_speedometer())
+
+        my_bike.brake()
+        self.assertEqual(23, my_bike.check_speedometer())
+
+    def test_bike_on_gear_3_speed_reduce_by_3(self):
+        my_bike = Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        for count in range(29):
+            my_bike.accelerate()
+        self.assertEqual(40, my_bike.check_speedometer())
+        self.assertEqual(3, my_bike.check_gear())
+
+        my_bike.brake()
+        self.assertEqual(37, my_bike.check_speedometer())
+
+        my_bike.brake()
+        my_bike.brake()
+        self.assertEqual(31, my_bike.check_speedometer())
+
+    def test_bike_on_gear_4_speed_reduce_by_4(self):
+        my_bike = Bike()
+        self.assertFalse(my_bike.check_status())
+        my_bike.start()
+        for count in range(33):
+            my_bike.accelerate()
+        self.assertEqual(55, my_bike.check_speedometer())
+        self.assertEqual(4, my_bike.check_gear())
+
+        my_bike.brake()
+        self.assertEqual(51, my_bike.check_speedometer())
+
+        my_bike.brake()
+        my_bike.brake()
+        self.assertEqual(43, my_bike.check_speedometer())
+        self.assertEqual(4, my_bike.check_gear())
